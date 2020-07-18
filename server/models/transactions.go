b1baccc78704d6df3ee310791080e6d5087d7018
hungryotter <hungryotter@pop-os.localdomain>
2020-07-18 01:49:53 -0500

    adding the postgres model

---
+++ server/models/transactions.go
@@ -0,0 +1,27 @@
+package models
+
+type TransactionModel struct {
+}
+
+type TransactionResponse struct {
+}
+
+func NewTransactionsModel() *TransactionModel {
+
+}
+
+func (e *TransactionModel) GetAllTransactions() {
+
+}
+
+func (e *TransactionModel) SearchTransaction(search, from, to string) {
+
+}
+
+func (e *TransactionModel) GetTransactionsInDateRange(search, from, to string) {
+
+}
+
+func (e *TransactionModel) CreateTransaction(data []byte, id string) {
+
+}
